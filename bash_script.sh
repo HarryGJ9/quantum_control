169667dd4fa731c0e9851ccc24e3cb6724e891df
Harry Jones <harry4gjones@gmail.com>
2024-02-27 15:10:43 +0000

    Various changes

--- bash_script.sh
+++ bash_script.sh
@@ -1,35 +1,36 @@
 #!/bin/bash
 
 # User provides genome sequence
-echo "Enter genome sequence: "
-read genome
+# echo "Enter genome sequence: "
+# read genome
 
-# Users specifies time at which to calculate fitness 
-echo "Enter time at which fitness is evaluated: "
-read time
+# # Users specifies time at which to calculate fitness 
+# echo "Enter time at which fitness is evaluated: "
+# read time
 
-# Time evolve the genome, calculating fidelity as a function of time
+# Calculate fidelity at specific time
 
-# Return fidelity at specified time
+
 
 # Compute the fitness function at that time
-a = 10
-b = -0.001
-t = time
-# F = fidelity 
-# Jmax = Jmax
+# a = 10
+# b = -0.001
+# t = time
+# # F = fidelity 
+# # Jmax = Jmax
 
-fitness = $(echo "scale=2; 100 * exp($a * ($F - 1)) * exp($b * $t * $Jmax)" | bc -1)
+# fitness = $(echo "scale=2; 100 * exp($a * ($F - 1)) * exp($b * $t * $Jmax)" | bc -1)
 
 # Perform gradient descent (perhaps from python script), calculating the fitness of the updated genome
-# at each stage of to guide the ascent/descent
+# at each stage to guide the ascent/descent
 
 
 ############################################
 # For gradient descent
 ############################################
 
-# genome_input = genome output from the genetic algorithm 
+genome_input = $(grep "best genome:" genetic.out | cut -d':' -f2)
+
 # threshold = threshold to determine when to cancel while loop based on how close the genome is to the final state
 
 # while |Fidelity_input_output - Fidelity_output_genome| > threshold:
