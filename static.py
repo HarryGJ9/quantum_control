040fda738fab6787be52df81216e936878ac048a
Harry Jones <harry4gjones@gmail.com>
2024-02-28 12:30:03 +0000

    Change

--- static.py
+++ static.py
@@ -1,18 +1,5 @@
-import os
+dynamics_path = r'/home/hgjones9/spinchain/output-latest/data/dynamics_formatted.data'
 
-# Get the directory of the current script
-current_directory = os.path.dirname(os.path.abspath(__file__))
+with open(dynamics_path, 'r') as file:
+    print(dynamics_path)
 
-# Specify the relative path to the data file
-file_path = os.path.join(current_directory, '../../spinchain/output-latest/data/dynamics_formatted.data')
-
-try:
-    with open(file_path, 'r') as file:
-        content = file.read()
-        print(content)
-except FileNotFoundError:
-    print(f"File '{file_path}' not found.")
-except Exception as e:
-    print("An error occurred:", e)
-
-    
