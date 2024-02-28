1644e0d77309f28ce118d01b9b24abb7f3e3e41e
Harry Jones <harry4gjones@gmail.com>
2024-02-28 11:54:11 +0000

    Var changes.

--- bash_script.sh
+++ bash_script.sh
@@ -35,26 +35,25 @@ output_directory="/home/hgjones9/spinchain/output-latest"
 genome_input=$(grep "best genome:" "$output_directory/genetic.out" | cut -d':' -f2)
 echo "$genome_input"
 
-# Define threshold to determine when to cancel the while loop based on how good the fidelity is
-threshold = 0.1
+# # Define threshold to determine when to cancel the while loop based on how good the fidelity is
+# threshold = 0.1
 
-# Fidelities of the input and output genomes
-fidelity_input = # fidelity of the input genome at a time T? 
-fidelity_output = # fidelity of the output genome at the same time T?
+# # Fidelities of the input and output genomes
+# fidelity_input = # fidelity of the input genome at a time T? 
+# fidelity_output = # fidelity of the output genome at the same time T?
 
-# Absolute difference between input and output fidelities
-absolute_diff = $(( (fidelity_input - fidelity_output) < 0 ? -(fidelity_input - fidelity_output) : (fidelity_input - fidelity_output) ))
+# # Absolute difference between input and output fidelities
+# absolute_diff = $(( (fidelity_input - fidelity_output) < 0 ? -(fidelity_input - fidelity_output) : (fidelity_input - fidelity_output) ))
 
 
+# while |absolute_diff > threshold:
 
-while |absolute_diff > threshold:
+# #   Fidelity_input_genome = calculate fidelity (at a specific time) for the input genome
+# #   call gradient descent code on the input genome; output genome_optimised
+# #   calculate dynamics of the output genome
+# #   calculate fidelity of the output genome 
 
-#   Fidelity_input_genome = calculate fidelity (at a specific time) for the input genome
-#   call gradient descent code on the input genome; output genome_optimised
-#   calculate dynamics of the output genome
-#   calculate fidelity of the output genome 
-
-# Genome_result = outputted genome_optimised
+# # Genome_result = outputted genome_optimised
 
 
 
