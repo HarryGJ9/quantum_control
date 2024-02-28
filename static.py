1641c82970c6ff8a1c0e8a69a989fc55cfe0dde7
Harry Jones <harry4gjones@gmail.com>
2024-02-28 12:32:44 +0000

    .

--- static.py
+++ static.py
@@ -1,5 +1,6 @@
 dynamics_path = r'/home/hgjones9/spinchain/output-latest/data/dynamics_formatted.data'
 
 with open(dynamics_path, 'r') as file:
-    print(file)
+    data = file.read()
+    print(data)
 
