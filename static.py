3379ec0d765fa6712d543ff40a6a8a74b98f7cf6
Harry Jones <harry4gjones@gmail.com>
2024-02-28 12:17:49 +0000

    Changed to correct for directory paths.

--- static.py
+++ static.py
@@ -1,7 +1,18 @@
+import os
 
-# IN UBUNTU, RUN FROM SPINCHAIN DIRECTORY
-file_path = r'/output-latest/data/dynamics_formatted.data'
+# Get the directory of the current script
+current_directory = os.path.dirname(os.path.abspath(__file__))
 
-with open(file_path, 'r') as file:
-    content=file.read()
-    print(content)
+# Specify the relative path to the data file
+file_path = os.path.join(current_directory, '../../spinchain/output-latest/data/dynamics_formatted.data')
+
+try:
+    with open(file_path, 'r') as file:
+        content = file.read()
+        print(content)
+except FileNotFoundError:
+    print(f"File '{file_path}' not found.")
+except Exception as e:
+    print("An error occurred:", e)
+
+    
