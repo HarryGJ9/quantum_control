841d49d714fe7fe9edf818043e6e9dce61ff430f
Harry Jones <harry4gjones@gmail.com>
2024-03-01 14:31:54 +0000

    .

--- genome_grad_descent.py
+++ genome_grad_descent.py
@@ -18,10 +18,10 @@ import re
 #############################
 
 # Directory of the output genome
-output_directory = r'home/hgjones9/spinchain/output-latest/genetic.out'
+output_path = r'/home/hgjones9/spinchain/output-latest/genetic.out'
 
 # Open genetic.out and find the genome
-with open('output_directory', 'r') as file:
+with open(output_path, 'r') as file:
     for line in file:
         if "best genome" in line:
             genome = line.split(':')[1].strip()
