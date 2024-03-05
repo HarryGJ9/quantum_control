cf892cced931ae9b7998393c8cd7ef6467d68de3
Harry Jones <harry4gjones@gmail.com>
2024-03-05 11:00:11 +0000

    obtain adjusted genome and run spinnet on each

--- bash_script.sh
+++ bash_script.sh
@@ -1,59 +1,30 @@
 #!/bin/bash
 
-# User provides genome sequence
-# echo "Enter genome sequence: "
-# read genome
+########################################################
+# OBTAIN ADJUSTED OUTPUT GENOMES FROM GENOME_ADJUSTER.PY
+########################################################
 
-# # Users specifies time at which to calculate fitness 
-# echo "Enter time at which fitness is evaluated: "
-# read time
+# Specify output file
+output_file = '/home/hgjones9/quantum_control/output.txt'
 
-# Calculate fidelity at specific time
+# Search output.txt for the list of adjusted genomes, return the list
+adjusted_genomes = $(grep -oP "Adjusted genomes = \['K[^']+" "$output_file")
 
+# Loop over the list and run spinnet on each genome
+for genome in $adjusted_genomes
+do
 
+    # Call spinnet for each genome
+    /home/hgjones9/spinchain/bin/spinnet "<A|C>$genome#00"
 
-# Compute the fitness function at that time
-# a = 10
-# b = -0.001
-# t = time
-# # F = fidelity 
-# # Jmax = Jmax
-
-# fitness = $(echo "scale=2; 100 * exp($a * ($F - 1)) * exp($b * $t * $Jmax)" | bc -1)
-
-# Perform gradient descent (perhaps from python script), calculating the fitness of the updated genome
-# at each stage to guide the ascent/descent
+done
 
 
-############################################
-# For gradient descent
-############################################
 
 
-# Genome input used in gradient descent, output of the GA
-output_directory="/home/hgjones9/spinchain/output-latest"
-genome_input=$(grep "best genome:" "$output_directory/genetic.out" | cut -d':' -f2)
-echo "$genome_input"
 
-# # Define threshold to determine when to cancel the while loop based on how good the fidelity is
-# threshold = 0.1
+# Calculate the fidelities of each genome by running spinnet
 
-# # Fidelities of the input and output genomes
-# fidelity_input = # fidelity of the input genome at a time T? 
-# fidelity_output = # fidelity of the output genome at the same time T?
-
-# # Absolute difference between input and output fidelities
-# absolute_diff = $(( (fidelity_input - fidelity_output) < 0 ? -(fidelity_input - fidelity_output) : (fidelity_input - fidelity_output) ))
-
-
-# while |absolute_diff > threshold:
-
-# #   Fidelity_input_genome = calculate fidelity (at a specific time) for the input genome
-# #   call gradient descent code on the input genome; output genome_optimised
-# #   calculate dynamics of the output genome
-# #   calculate fidelity of the output genome 
-
-# # Genome_result = outputted genome_optimised
 
 
 
