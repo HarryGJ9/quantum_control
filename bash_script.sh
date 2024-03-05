7a6997b85f039497652f7644c6ebc59d8bdb7596
Harry Jones <harry4gjones@gmail.com>
2024-03-05 11:37:13 +0000

    updated output path

--- bash_script.sh
+++ bash_script.sh
@@ -11,14 +11,14 @@ read initial_genome
 # Run spinnet -o on an initial genome 
 /home/hgjones9/spinchain/bin/spinnet -o "$initial_genome"
 
-# # Obtain the optimised output genome and make changes using genome_adjuster.py
-# python3 /home/hgjones9/quantum_control/genome_adjuster.py
+# Obtain the optimised output genome and make changes using genome_adjuster.py
+python3 /home/hgjones9/quantum_control/genome_adjuster.py
 
-# # Specify output file of adjusted genomes
-# output_file='/home/hgjones9/quantum_control/output.txt'
+# Specify output file of adjusted genomes
+output_file='/home/hgjones9/quantum_control/output.txt'
 
-# # Search output.txt for the list of adjusted genomes, return the list
-# adjusted_genomes=$(grep -oP "Adjusted genomes = \['K[^']+" "$output_file")
+# Search output.txt for the list of adjusted genomes, return the list
+adjusted_genomes=$(grep -oP "Adjusted genomes = \['K[^']+" "$output_file")
 
 # # Loop over the list and run spinnet on each genome
 # for genome in $adjusted_genomes
