57294b4d3b49c87817274c315d2c97f161029c48
Harry Jones <harry4gjones@gmail.com>
2024-03-05 11:43:19 +0000

    .

--- bash_script.sh
+++ bash_script.sh
@@ -20,13 +20,13 @@ output_file='/home/hgjones9/quantum_control/output.txt'
 # Search output.txt for the list of adjusted genomes, return the list
 adjusted_genomes=$(grep -oP "Adjusted genomes = \['K[^']+" "$output_file")
 
-# # Loop over the list and run spinnet on each genome
-# for genome in $adjusted_genomes
-# do
-#     # Call spinnet for each genome
-#     /home/hgjones9/spinchain/bin/spinnet "<A|C>$genome#00"
+# Loop over the list and run spinnet on each genome
+for genome in $adjusted_genomes
+do
+    # Call spinnet for each genome
+    /home/hgjones9/spinchain/bin/spinnet "<A|C>$genome#00"
 
-# done
+done
 
 
 
