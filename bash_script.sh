41eba947743810d0058038e28fd4cd1adb753957
Harry Jones <harry4gjones@gmail.com>
2024-04-08 16:30:50 +0100

    .

--- bash_script.sh
+++ bash_script.sh
@@ -30,35 +30,35 @@ adjusted_genomes=$(grep -oP "Adjusted genomes = \[\K.*(?=\])" "$output_file")
 # Print the list of adjusted genomes
 echo "$adjusted_genomes"
 
-cd /home/hgjones9/spinchain
-pwd
+# # cd /home/hgjones9/spinchain
+# # pwd
 
-##################################################
-# RUN SPINNET ON EACH GENOME TO CALCUALTE DYNAMICS
-##################################################
+# ##################################################
+# # RUN SPINNET ON EACH GENOME TO CALCUALTE DYNAMICS
+# ##################################################
 
-# Loop over the list of adjusted genoms and run spinnet on each genome
-for string in $adjusted_genomes
-do
-    genome=$(echo "$string" | sed "s/'\([^']*\)'.*/\1/") # Remove the individual quotation marks from each genome
+# # Loop over the list of adjusted genoms and run spinnet on each genome
+# for string in $adjusted_genomes
+# do
+#     genome=$(echo "$string" | sed "s/'\([^']*\)'.*/\1/") # Remove the individual quotation marks from each genome
 
-    # Call spinnet for each genome, generating a different output directory for each genome
-    bin/spinnet "<A|C>$genome"
-    echo "<A|C>$genome"
-done
+#     # Call spinnet for each genome, generating a different output directory for each genome
+#     /home/hgjones9/spinchain/bin/spinnet "<A|C>$genome"
+#     echo "<A|C>$genome"
+# done
 
 
-###########################################################################
-# CALCULATE THE GRADIENT VECTOR OF THE FIDELITY WITH RESPECT TO THE GENOMES
-###########################################################################
+# ###########################################################################
+# # CALCULATE THE GRADIENT VECTOR OF THE FIDELITY WITH RESPECT TO THE GENOMES
+# ###########################################################################
 
-python3 /home/hgjones9/quantum_control/calculate_gradients.py
+# python3 /home/hgjones9/quantum_control/calculate_gradients.py
 
-############################################
-# CALCULATE NEW COUPLINGS BY GRADIENT ASCENT
-############################################
+# ############################################
+# # CALCULATE NEW COUPLINGS BY GRADIENT ASCENT
+# ############################################
 
-python3 /home/hgjones9/quantum_control/update_genome.py
+# python3 /home/hgjones9/quantum_control/update_genome.py
 
 
 
