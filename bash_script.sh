1fad8654560cd7dc936c2cf9268c0ee0c2e48c2f
Harry Jones <harry4gjones@gmail.com>
2024-04-12 11:27:12 +0100

    .

--- bash_script.sh
+++ bash_script.sh
@@ -18,58 +18,62 @@ initial_genome="<A|C>AB500BC500"
 # Run spinnet -o on an initial genome 
 /home/hgjones9/spinchain/bin/spinnet -o "$initial_genome"
 
-# RETRIEVE THE TIME AND MAX FIDELITY FROM GENOME.OUT
-
-# At this point, there is an output file located at /home/hgjones9/quantum_control/output_latest/genetic.out"
-# This file contains the optimised genome "<i|f>AB...BC..."
-
 cd /home/hgjones9/quantum_control
 pwd
 
-# Obtain the optimised output genome and make changes using genome_adjuster.py
-python3 /home/hgjones9/quantum_control/initial_genome_adjuster.py
 
-# Specify output file of adjusted genomes
-output_file='/home/hgjones9/quantum_control/initial_adjusted_genomes.txt'
-
-# Search output.txt for the line containing the list of adjusted genomes and print them as a list
-adjusted_genomes=$(grep -oP "Adjusted genomes : \[\K.*(?=\])" "$output_file")
-
-# Print the list of adjusted genomes
-echo "$adjusted_genomes"
-
-#####################################################
-# OBTAIN INITIAL COUPLINGS, READY FOR GRADIENT ASCENT
-#####################################################
-
-python3 /home/hgjones9/quantum_control/extract_initial_couplings.py
-
-#######################################################################
-# RENAME 'initial_couplings.txt' TO 'old_couplings.txt' FOR GRAD ASCENT
-#######################################################################
-
-mv initial_couplings.txt old_couplings.txt
-
-##################################################
-# RUN SPINNET ON EACH GENOME TO CALCUALTE DYNAMICS
-##################################################
-
-# Loop over the list of adjusted genomes and run spinnet on each genome
-for string in $adjusted_genomes
-do
-    genome=$(echo "$string" | sed "s/'\([^']*\)'.*/\1/") # Remove the individual quotation marks from each genome
-
-    # Call spinnet for each genome, generating a different output directory for each genome
-    /home/hgjones9/spinchain/bin/spinnet "<A|C>$genome"
-    echo "<A|C>$genome"
-done
+# Retrieve max fidelity of optimised genome and time at which it occurs
+/max_fidelity_time.py
 
 
-###########################################################################
-# CALCULATE THE GRADIENT VECTOR OF THE FIDELITY WITH RESPECT TO THE GENOMES
-###########################################################################
+# # At this point, there is an output file located at /home/hgjones9/quantum_control/output_latest/genetic.out"
+# # This file contains the optimised genome "<i|f>AB...BC..."
 
-python3 /home/hgjones9/quantum_control/calculate_gradients.py
+
+# # Obtain the optimised output genome and make changes using genome_adjuster.py
+# python3 /home/hgjones9/quantum_control/initial_genome_adjuster.py
+
+# # Specify output file of adjusted genomes
+# output_file='/home/hgjones9/quantum_control/initial_adjusted_genomes.txt'
+
+# # Search output.txt for the line containing the list of adjusted genomes and print them as a list
+# adjusted_genomes=$(grep -oP "Adjusted genomes : \[\K.*(?=\])" "$output_file")
+
+# # Print the list of adjusted genomes
+# echo "$adjusted_genomes"
+
+# #####################################################
+# # OBTAIN INITIAL COUPLINGS, READY FOR GRADIENT ASCENT
+# #####################################################
+
+# python3 /home/hgjones9/quantum_control/extract_initial_couplings.py
+
+# #######################################################################
+# # RENAME 'initial_couplings.txt' TO 'old_couplings.txt' FOR GRAD ASCENT
+# #######################################################################
+
+# mv initial_couplings.txt old_couplings.txt
+
+# ##################################################
+# # RUN SPINNET ON EACH GENOME TO CALCUALTE DYNAMICS
+# ##################################################
+
+# # Loop over the list of adjusted genomes and run spinnet on each genome
+# for string in $adjusted_genomes
+# do
+#     genome=$(echo "$string" | sed "s/'\([^']*\)'.*/\1/") # Remove the individual quotation marks from each genome
+
+#     # Call spinnet for each genome, generating a different output directory for each genome
+#     /home/hgjones9/spinchain/bin/spinnet "<A|C>$genome"
+#     echo "<A|C>$genome"
+# done
+
+
+# ###########################################################################
+# # CALCULATE THE GRADIENT VECTOR OF THE FIDELITY WITH RESPECT TO THE GENOMES
+# ###########################################################################
+
+# python3 /home/hgjones9/quantum_control/calculate_gradients.py
 
 # ############################################
 # # CALCULATE NEW COUPLINGS BY GRADIENT ASCENT
---
+++ max_fidelity_time.py
@@ -0,0 +1,31 @@
+# Get maximum fidelity and the time of max fidelity from the first genetic.out file
+
+# genetic.out path
+genetic_out_path = '/home/hgjones9/quantum_control/output-latest/genetic.out'
+
+with open(genetic_out_path, 'r') as file:
+
+    # Initialise line count to track the number of lines starting with 'with fitness'
+    line_count = 0
+    for line in file:
+        if line.startswith('with fitness'):
+            # If the line starts with 'with fitness', increment the line count by 1
+            line_count += 1
+            if line_count == 2:
+                # If it is the second line starting with 'with fitness', split the line into a list
+                line_split = line.split()
+                # Look for the indices of the list containing the strings 'fidelity' and 'time'
+                fidelity_idx = line_split.index('fidelity')
+                time_idx = line_split.index('time')
+                # Extract the values before 'fidelity' and after 'time'
+                fidelity_val = line_split[fidelity_idx - 1]
+                time_val = line_split[time_idx + 1]
+
+                # Remove '%' from fidelity_val
+                fidelity_val = fidelity_val.replace('%', '')
+    
+    # Print values to check
+    print(f'Maximum fidelity of GA optimised genome: {fidelity_val}')
+    print(f'Time at maximum fidelity: {time_val}')
+
+    
