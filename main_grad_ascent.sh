05b2658acd3e8e673bf6c125c4afd045653ca3eb
Harry Jones <harry.jones@bristol.ac.uk>
2024-08-29 12:11:46 +0100

    Initial commits

---
+++ main_grad_ascent.sh
@@ -0,0 +1,15 @@
+# This file exectutes gradient ascent on 5 different genetically optimised genomes and outputs the one with highest fidelity.
+
+#!/bin/bash
+
+# Enter initial genome
+
+# Run (Carrie's) spinnet for a certain number of generations to output 5 genetically optimised genomes
+
+
+# Setup loop which executes gradient ascent over each genome
+
+# Output the 5 optimised genomes, pick the best one and plot
+
+
+
